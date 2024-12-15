384a295b8dfb83171d57126a3fe8b7801b868c01
Guru-Devops-creator <gurut591@gmail.com>
2024-12-15 14:02:06 +0530

    elseif

---
+++ Conditions/variables.tf
@@ -0,0 +1,22 @@
+variable "ami_id" {
+  default     = "ami-09c813fb71547fc4f"
+  description = "This is ami id of devops group"
+}
+
+variable "instance_type" {
+  # default = "t3.micro"
+}
+variable "tags" {
+  default     = {
+    Name = "terraform"
+    Project = "expense"
+    Environment = "dev"
+    
+  }
+  
+}
+
+variable "environment" {
+  default     = "prod"
+
+}
